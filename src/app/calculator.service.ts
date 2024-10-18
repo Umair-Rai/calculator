efeb87342f5f9c105de818d09c537c59b41e2d7b
Umair-Rai <umair.akramcs@gmail.com>
2024-10-18 20:44:58 +0500

    Serves/Calculator
    Add files for the logic of Calculator

---
+++ src/app/calculator.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculatorService {
+
+  constructor() { }
+
+  // Basic arithmetic operations
+
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  subtract(a: number, b: number): number {
+    return a - b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  divide(a: number, b: number): number {
+    if (b === 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    return a / b;
+  }
+}
